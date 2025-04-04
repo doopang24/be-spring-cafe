504c9fe77b141c4476b3ae6f73aeb89720a6e30f
doopang24 <wodus4145@naver.com>
2025-04-04 11:06:09 +0900

    feat: 게시글을 조회 시 댓글 포함하도록 쿼리 추ㄱ가

--- src/main/java/codesquad/codestagram/repository/SpringDataJpaArticleRepository.java
+++ src/main/java/codesquad/codestagram/repository/SpringDataJpaArticleRepository.java
@@ -2,10 +2,21 @@ package codesquad.codestagram.repository;
 
 import codesquad.codestagram.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SpringDataJpaArticleRepository extends JpaRepository<Article, Long> {
 
     List<Article> findByDeletedFalse();
+
+    @Query("""
+        SELECT DISTINCT a FROM Article a
+        LEFT JOIN FETCH a.replies
+        WHERE a.id = :id AND a.deleted = false
+    """)
+    Optional<Article> findByIdWithReplies(@Param("id") Long id);
+    // LEFT JOIN FETCH 쓰면 댓글이 없어도 게시글 정상적으로 조회
 }
